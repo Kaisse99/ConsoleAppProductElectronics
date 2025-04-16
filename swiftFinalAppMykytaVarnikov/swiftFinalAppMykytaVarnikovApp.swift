354f3e82a93d1ff2081fa9cdfdf068fc703914e0
Mykyta Varnikov <varnikov@sheridancollege.ca>
2025-04-16 11:45:23 -0400

    Initial Commit

---
+++ swiftFinalAppMykytaVarnikov/swiftFinalAppMykytaVarnikovApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftFinalAppMykytaVarnikovApp.swift
+//  swiftFinalAppMykytaVarnikov
+//
+//  Created by Mykyta Kaisenberg on 2025-04-16.
+//
+
+import SwiftUI
+
+@main
+struct swiftFinalAppMykytaVarnikovApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
